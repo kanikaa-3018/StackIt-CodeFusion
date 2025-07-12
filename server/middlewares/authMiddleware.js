c93181e3e444a80897c70a7a70fb381c7fa986b8
SidrahAaishah <sidrahaaishah1@gmail.com>
2025-07-12 15:56:07 +0530

    modified middleware to work correctlt for answers

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,3 +1,26 @@
+// import jwt from "jsonwebtoken";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const protect = (req, res, next) => {
+//   console.log('req.user:', req.user);
+// console.log('userId:', userId);
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader?.startsWith("Bearer")) {
+//     return res.status(401).json({ error: "Unauthorized" });
+//   }
+
+//   const token = authHeader.split(" ")[1];
+//   try {
+//     const payload = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = payload;
+//     next();
+//   } catch {
+//     return res.status(401).json({ error: "Invalid token" });
+//   }
+// };
+
+
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../models/User.js"; // ✅ import your User model
