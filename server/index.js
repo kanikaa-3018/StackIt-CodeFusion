7b709518af14a960f9cd7b9b79cd74ce8994275c
SidrahAaishah <sidrahaaishah1@gmail.com>
2025-07-12 11:27:14 +0530

    Answer Controller added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import questionRoutes from './routes/questionRoutes.js'
+import answerRoutes from './routes/answerRoute.js';
 import cors from 'cors';
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/questions', questionRoutes);
+app.use('/api/answers', answerRoutes);
 
 // Root route
 app.get('/', (req, res) => {
